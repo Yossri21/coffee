9a56790241a0bcb4c0953171daae21afe00c5332
Yosri21 <yosriasfour@gmail.com>
2020-04-26 17:38:17 +0100

    basic frontend

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,8 +10,14 @@ import PropTypes from "prop-types"
 import "./bootstrap.min.css"
 import "./layout.css"
 
+import Navbar from './Globals/Navbar'
+
+import Footer from "./Globals/Footer";
+
 const Layout = ({ children }) => <>
+<Navbar />
 {children}
+<Footer />
  </>
 
 Layout.propTypes = {
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { Link, graphql } from "gatsby";
+
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import BackgroundSection from "../components/Globals/BackgroundSection";
 import Info from "../components/Home/Info";
-const IndexPage = ({ data }) => (
+const AboutPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <BackgroundSection
       img={data.img.childImageSharp.fluid}
-      title="regular joe's"
+      title="about us"
+      styleClass="about-background"
     />
     <Info />
   </Layout>
@@ -17,7 +19,7 @@ const IndexPage = ({ data }) => (
 
 export const query = graphql`
   {
-    img: file(relativePath: { eq: "default-background.jpeg" }) {
+    img: file(relativePath: { eq: "about-background.jpeg" }) {
       childImageSharp {
         fluid {
           ...GatsbyImageSharpFluid_tracedSVG
@@ -27,4 +29,4 @@ export const query = graphql`
   }
 `;
 
-export default IndexPage;
+export default AboutPage;
