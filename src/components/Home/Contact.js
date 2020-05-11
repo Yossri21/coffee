7a275fcaeabcf9c264635729610a429f4310c3c8
Yosri21 <yosriasfour@gmail.com>
2020-05-11 17:07:41 +0100

    update contact

--- src/components/Home/Contact.js
+++ src/components/Home/Contact.js
@@ -28,7 +28,7 @@ export default function Contact() {
                 className="form-control"
                 name="email"
                 id="email"
-                placeholder="email@email"
+                placeholder="email@email.email"
               />
             </div>
             {/* description */}
